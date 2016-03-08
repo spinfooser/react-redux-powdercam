046e7a4f967654ee95dd8c8f2dded7446445adf4
Josh Mattila <jmattila@pivotal.io>
2016-03-07 17:03:54 -0700

    Adding a client side image update scheme.
    Not sure I'm super happy with it yet though. It uses
    the img tag and updates the source tag to get new images.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-redux-powdercam
-Playing around with React
+Playing around with React.
+
+Future goals:
+
+* Add redux to the project to handle AJAX requests for next stage.
+* Integrate with a backend that will serve images to client application.
+  This is needed because CORS blocks client-side image requests
--- src/winterPark/winterParkComponent.jsx
+++ src/winterPark/winterParkComponent.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import ReactTimerMixin from 'react-timer-mixin';
 
 export default class WinterParkCamera extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {image_src: "http://cams.winterparkresort.com/snow-stake-cam.jpg"};
+    }
+
+    componentDidMount() {
+        this.timer = setInterval(() => this.tick.apply(this), 12000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timer);
+    }
+
+    tick() {
+        this.setState({image_src: "http://cams.winterparkresort.com/snow-stake-cam.jpg?" + Date.now()});
+    }
+
+
     render() {
         return (
-            <div><p>Winter Park Image!</p></div>
+            <div>
+                <p>
+                    Winter Park Image!
+                </p>
+                <img src={this.state.image_src}/>
+            </div>
         );
     };
 }
